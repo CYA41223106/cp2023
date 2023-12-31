4dbb2761509b4799bf2c55accbe23524d5e72412
CYA41223106 <144768512+CYA41223106@users.noreply.github.com>
2023-12-31 08:56:44 +0000

    新增1.c

---
+++ downloads/1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()  
+{
+    // Print Name
+    printf("Name   :陳怡安\n"); 
+
+    // Print Date of Birth
+    printf("DOB    : August 18, 2005\n"); 
+
+    // Print Mobile Number
+    printf("Mobile : 09-67255656\n"); 
+
+    // Indicate successful execution
+    return(0); 
+}
